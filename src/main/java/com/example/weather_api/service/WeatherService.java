38899f8b7ed4effc61eb6386581520123bb33f7b
Vishnevskaya Elena <elvishn475@gmail.com>
2026-03-09 14:52:11 +0300

    delete cityCode()

--- src/main/java/com/example/weather_api/service/WeatherService.java
+++ src/main/java/com/example/weather_api/service/WeatherService.java
@@ -19,19 +19,8 @@ import java.util.stream.StreamSupport;
 @AllArgsConstructor
 public class WeatherService {
     private final WebClient webClient;
-    static final String CITY_URL_TEMPLATE = "/locations/v1/cities/search?q=Zermatt";
-    static final String NOW_URL_TEMPLATE = "/currentconditions/v1/315560";
+    static final String NOW_URL_TEMPLATE = "/currentconditions/v1/315560"; // weather in Zemmart
 
-    public  Mono<String> cityCode() {
-        return webClient.get()
-                .uri(CITY_URL_TEMPLATE)
-                .retrieve()
-                .bodyToMono(JsonNode.class)
-                .map(jsonNode -> {
-                    String code = jsonNode.get(0).path("Key").asText(null);
-                    return code;
-                });
-    }
 
     public Mono<WeatherNowData> getNowWeather() {
         return webClient.get()
